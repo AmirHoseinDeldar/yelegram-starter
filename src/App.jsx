a2a88fa16e01c6c2dac07be2d55a3220d0875286
AmirHosein Deldar <Amirhoseindeldar@yahoo.com>
2024-10-23 23:51:07 +0330

    start

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+
+      <div className="font-mono min-h-screen flex items-center justify-center">
+        <h1 className="text-7xl font-bold">بزن بریم ستونم</h1>
+      </div>
+    
+    </>
+  );
+}
+
+export default App;
